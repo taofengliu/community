e519d47bf116f0658f1bd76e759536d8ec295356
SingerPigMan <taofeng_liu@outlook.com>
2021-05-23 22:16:44 +0800

    init repo

---
+++ src/main/java/com/liu/community/JavaCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.liu.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liu/community/JavaCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
